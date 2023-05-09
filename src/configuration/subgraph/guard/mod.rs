178c0c20bb5cbde8e79356fb2b4bd61684b7a983
Nick McLean <nickisyourfan@gmail.com>
2023-05-08 20:56:04 -0500

    44 - guards

--- src/configuration/subgraph/guard/mod.rs
+++ src/configuration/subgraph/guard/mod.rs
@@ -1,8 +1,8 @@
 use async_graphql::{Error, ErrorExtensions};
-use bson::Document;
+use bson::{Bson, Document};
 use evalexpr::*;
 use http::HeaderMap;
-use log::debug;
+use log::{debug, error};
 use serde::{Deserialize, Serialize};
 
 use crate::configuration::subgraph::entities::{ScalarOptions, ServiceEntity};
@@ -25,7 +25,9 @@ impl Guard {
             let should_guard = eval_boolean_with_context(guard.if_expr.as_str(), guard_context);
             debug!("Should Guard: {:?}", should_guard);
             if should_guard.is_err() {
-                return Err(Error::new("Guard Creation Error"));
+                error!("Guard Creation Error, {:?}", should_guard);
+                return Err(Error::new("Guard Creation Error")
+                    .extend_with(|_err, e| e.set(guard.name.clone(), "Required when guarding.")));
             }
 
             if should_guard.unwrap() {
@@ -48,6 +50,61 @@ impl Guard {
         Ok(())
     }
 
+    pub fn get_from_document(
+        input_document: Document,
+        cleaned_key: String,
+        scalar: ScalarOptions,
+        is_list: bool,
+        argument: &Value,
+    ) -> Result<Value, EvalexprError> {
+        debug!("Getting Value from Document");
+        debug!("Document: {:?}", input_document);
+        debug!("Key: {:?}", cleaned_key);
+        debug!("Scalar: {:?}", scalar);
+        debug!("Is List: {:?}", is_list);
+        if is_list {
+            if let Some(Bson::Array(documents)) = input_document.get(&cleaned_key) {
+                let mut values = Vec::new();
+                for document in documents {
+                    match scalar {
+                        ScalarOptions::String | ScalarOptions::ObjectID => {
+                            println!("Document: {:?}", document);
+                            let cleaned_string = document.to_string().replace("\"", "");
+                            values.push(Value::String(cleaned_string))
+                        }
+                        ScalarOptions::Int => {
+                            values.push(Value::Int(document.as_i32().unwrap() as i64))
+                        }
+                        ScalarOptions::Boolean => {
+                            values.push(Value::Boolean(document.as_bool().unwrap()))
+                        }
+                        _ => unreachable!(),
+                    }
+                }
+                debug!("Values: {:?}", values);
+                Ok(Value::from(values))
+            } else {
+                return Err(EvalexprError::expected_string(argument.clone()));
+            }
+        } else {
+            match input_document.clone().get(&cleaned_key) {
+                Some(v) => {
+                    match scalar {
+                        ScalarOptions::String | ScalarOptions::ObjectID => {
+                            Ok(Value::String(v.to_string()))
+                        }
+                        ScalarOptions::Int => Ok(Value::Int(v.as_i32().unwrap() as i64)),
+                        ScalarOptions::Boolean => Ok(Value::Boolean(v.as_bool().unwrap())),
+                        //TODO: Implement Object scalars
+                        _ => unreachable!(),
+                    }
+                }
+                //TODO: Eval Scalar Error
+                None => Err(EvalexprError::expected_string(argument.clone())),
+            }
+        }
+    }
+
     pub fn create_guard_context<'a>(
         headers: HeaderMap,
         input_document: Document,
@@ -57,26 +114,16 @@ impl Guard {
 
         let context = context_map! {
             "input" => Function::new(move |argument| {
+                debug!("Input Argument: {:?}", argument);
                 let key = argument.to_string();
                 let cleaned_key = key.replace("\"", "");
                 let field = ServiceEntity::get_field(&entity.clone(), &cleaned_key);
                 if field.is_none() {
                     return Err(EvalexprError::expected_string(argument.clone()))
                 }
-                let scalar = field.unwrap().scalar;
-                match input_document.clone().get(&cleaned_key) {
-                    Some(v) => {
-                        match scalar {
-                            ScalarOptions::String | ScalarOptions::ObjectID => Ok(Value::String(v.to_string())),
-                            ScalarOptions::Int => Ok(Value::Int(v.as_i32().unwrap() as i64)),
-                            ScalarOptions::Boolean => Ok(Value::Boolean(v.as_bool().unwrap())),
-                            //TODO: Implement Object scalars
-                            _ => unreachable!()
-                        }
-                    },
-                        //TODO: Eval Scalar Error
-                    None => Err(EvalexprError::expected_string(argument.clone()))
-                }
+                let scalar = field.clone().unwrap().scalar;
+                let is_list = field.unwrap().list.unwrap_or(false);
+                Guard::get_from_document(input_document.clone(), cleaned_key, scalar, is_list, argument)
             }),
             "headers" => Function::new(move |argument| {
                 let key = argument.to_string();
--- src/graphql/mod.rs
+++ src/graphql/mod.rs
@@ -1,2 +1,1 @@
-pub mod scalars;
 pub mod schema;
