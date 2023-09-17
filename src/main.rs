b25d90813fc8c383aebb22b01458b8c4d951462b
Nick <nmclean@cycle.io>
2023-09-17 14:44:06 -0500

    60 - replace env vars fixed

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,6 @@ async fn main() -> Result<(), std::io::Error> {
     let environment = Environment::new();
     let mut subgraph_config = SubGraphConfig::new(&args);
     subgraph_config = Environment::replace_env_vars_in_config(subgraph_config, environment);
-
     utils::logger::Logger::init(&args, &subgraph_config);
     Ok(run(args, subgraph_config).await?.0.await)
 }
